82577bd982eb7126cd68904293b20457d40ed683
Kaderking <skkader024@gmail.com>
2024-11-28 17:29:09 +0600

    Create Name.sh

---
+++ Name.sh
@@ -0,0 +1,39 @@
+figlet gray  hat
+read -p "Enter the last three numbers of your I>
+
+case $you in
+  720)
+
+    echo "Name: Kader"
+    echo "Father's Name: "
+    echo "Mother's Name: "
+    echo "Permanent Address: "
+    echo "Present Address: "
+    echo "Date of Birth: "
+    echo "NID Number: "
+    echo "Religion: "
+    echo "Nationality: "
+;;
+308)
+    echo "Name: Rubel"
+    echo "Father's Name: "
+    echo "Mother's Name: "
+    echo "Permanent Address: "
+    echo "Present Address: "
+    echo "Date of Birth: "
+    echo "NID Number: "
+    echo "Religion: "
+    echo "Nationality:"
+;;
+725)
+    echo "Name: Saiful"
+    echo "Father's Name: "
+    echo "Mother's Name: "
+    echo "Permanent Address: "
+    echo "Present Address: "
+;;
+  *)
+
+    echo "You entered the wrong number"
+    ;;
+esac
